1a1ce08b93a37b9394fc996e6b3910052ba4181a
Nicolas Figay <nicolas.figay@gmail.com>
2018-01-02 14:59:26 +0100

    event span plugged

--- ArchimateDMNInteractiveViewer/js/ArchiMateDmnViewer.js
+++ ArchimateDMNInteractiveViewer/js/ArchiMateDmnViewer.js
@@ -27,6 +27,7 @@ function initArchiMate(){
   loadArchi();
 }
 
+
 function loadArchi()
 {
   
@@ -676,3 +677,4 @@ function onClickModelNodes(params){
   params.event = "[original event]";
   document.getElementById('eventSpan').innerHTML = '<h2>Click event:</h2>' + JSON.stringify(params, null, 4);
   console.log('click event, getNodeAt returns: ' + this.getNodeAt(params.pointer.DOM));
+}
