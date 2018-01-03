042543b84635789a25874b1cded8fc9dbe9e23fd
Nicolas Figay <nicolas.figay@gmail.com>
2018-01-03 15:02:30 +0100

    viewpoint filtering

--- ArchimateDMNInteractiveViewer/js/ArchiMateDmnViewer.js
+++ ArchimateDMNInteractiveViewer/js/ArchiMateDmnViewer.js
@@ -696,6 +696,7 @@ function createVis(model){
       +'}';
       node=JSON.parse(nodeString);
       if (document.getElementById("filterViewpoint").checked==true){
+        alert (viewpointId);
         if (document.getElementById(viewpointId).checked==true){ nodes.push(node);}
         else{ nodes.push(node);}
       }
