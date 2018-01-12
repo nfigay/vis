6f90fb71d4eecde0582ef76351366e0cbb60a7a0
Nicolas Figay <nicolas.figay@gmail.com>
2018-01-12 14:17:12 +0100

    datasets 018

--- ArchimateDMNInteractiveViewer/js/ArchiMateDmnViewer.js
+++ ArchimateDMNInteractiveViewer/js/ArchiMateDmnViewer.js
@@ -1073,7 +1073,7 @@ if (displayViews){
             node=JSON.parse(nodeString);
             console.log ("3");
             if (elements[j].getAttribute('id')=="f88bd846-6ea9-46be-ba08-a6f1498cf44d"){
-              console.log (elements[j].getAttribute('id').parentElement.id);
+              console.log (elements[j].getAttribute('id').parentNode.id);
             }
 
             loadedData.add([{
