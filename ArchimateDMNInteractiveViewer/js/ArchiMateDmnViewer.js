58017a7d0418426cc2bcbd3da64f230345304490
Nicolas Figay <nicolas.figay@gmail.com>
2018-01-20 10:13:44 +0100

    update 006

--- ArchimateDMNInteractiveViewer/js/ArchiMateDmnViewer.js
+++ ArchimateDMNInteractiveViewer/js/ArchiMateDmnViewer.js
@@ -826,11 +826,15 @@ function createVis(model){
 
 
 
-  if (document.getElementById("displayStakehloder").checked==true || document.getElementById("displayViewpoint").checked==true){}
+  if (document.getElementById("displayStakeholder").checked==true || document.getElementById("displayViewpoint").checked==true){}
   else {
     for (i = 0; i < W4S.length; i++){
       for (j = 0; j < W4S[i].length; j++){
-       edges.push({from: Viewpoints[i].toLowerCase().replace (" and ", "_").replace(" ","_"), to: stakeholders[i][j].toLowerCase().replace(" ","_"),  arrows:'to', length: EDGE_LENGTH_MAIN, label:"concerns", title:"concerns"});
+        var stakeholderId=Stakeholders[i].toLowerCase().replace(" ","_")+"_stakeholder";
+        var viewpointId=Viewpoints[i].toLowerCase().replace (" and ", "_").replace(" ","_").replace(" ","_")+"_viewpoint";
+        if (document.getElementById(viewpointId).checked==true && document.getElementById(stakeholderId).checked==true){
+          edges.push({from: Viewpoints[i].toLowerCase().replace (" and ", "_").replace(" ","_"), to: stakeholders[i][j].toLowerCase().replace(" ","_"),  arrows:'to', length: EDGE_LENGTH_MAIN, label:"concerns", title:"concerns"});
+        }
       }
     } 
   }
