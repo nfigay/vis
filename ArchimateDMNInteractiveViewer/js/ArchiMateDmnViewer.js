6e496faef9d52fcd37fd15efecf87f0e813e46c2
Nicolas Figay <nicolas.figay@gmail.com>
2018-01-31 11:14:03 +0100

    view folder displayed if displayView true

--- ArchimateDMNInteractiveViewer/js/ArchiMateDmnViewer.js
+++ ArchimateDMNInteractiveViewer/js/ArchiMateDmnViewer.js
@@ -592,6 +592,7 @@ function initArchiMate(){
   document.getElementById("displaySVGButton").style.backgroundColor="lightgray";
   document.getElementById("filterStakeholder").checked=false;
   document.getElementById("filterViewpoint").checked=false;
+  document.getElementById("displayFolders").checked=false;
   loadArchi();
 }
 
