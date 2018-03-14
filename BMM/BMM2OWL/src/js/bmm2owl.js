882a31a823008e6f87f8b5b1f0d40975645bbc67
Nicolas Figay <nicolas.figay@gmail.com>
2018-03-14 14:42:38 +0100

    BMM2OWL 014

--- BMM/BMM2OWL/src/js/bmm2owl.js
+++ BMM/BMM2OWL/src/js/bmm2owl.js
@@ -115,8 +115,8 @@ function loadBMMOWL()
  {
     loadedData=null;
     loadedData=new vis.DataSet(loadedDataOptions);
-    var queryAssociations = data.evaluate( '//packagedElement[@xmi:type="uml:Association"]', data, nsResolver,XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null );  
-    var nbassociations = data.evaluate( 'count(//packagedElement[@xmi:type="uml:Association"])', data, nsResolver, XPathResult.ANY_TYPE, null );
+    var queryAssociations = data.evaluate( '/packagedElement[@xmi:type="uml:Association"]', data, nsResolver,XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null );  
+    var nbassociations = data.evaluate( 'count(/packagedElement[@xmi:type="uml:Association"])', data, nsResolver, XPathResult.ANY_TYPE, null );
   
     console.log ("ok"+nbassociations.value);
     for (i = 0; i < queryAssociations.snapshotLength; i++) {
