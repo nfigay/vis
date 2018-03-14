be4e9242a5ec5a11623babd581998d2ee878c8c6
Nicolas Figay <nicolas.figay@gmail.com>
2018-03-14 14:03:48 +0100

    BMM2OWL 000

--- BMM/BMM2OWL/src/js/bmm2owl.js
+++ BMM/BMM2OWL/src/js/bmm2owl.js
@@ -118,7 +118,7 @@ function loadBMMOWL()
     var queryAssociations = data.evaluate( '//packagedElement[@mxi:type="uml:Association"]', data, nsResolver,XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null );  
     for (i = 0; i < queryAssociations.snapshotLength; i++) {
         //views.push(queryAssociations.snapshotItem(i));
-        
+
        console.log(queryAssociations.snapshotItem(i));
     }
 
@@ -128,6 +128,9 @@ console.log ("processXMI");
 function nsResolver(prefix) {
     switch(prefix){
       case "xsi":return 'http://www.w3.org/2001/XMLSchema-instance';
+      case "xmi":return 'http://www.omg.org/spec/XMI/20110701';
+      case "uml":return 'http://www.omg.org/spec/UML/20110701';
+      case "mofext":return 'http://www.omg.org/spec/MOF/20110701';
       default: return null;
     }
 }
